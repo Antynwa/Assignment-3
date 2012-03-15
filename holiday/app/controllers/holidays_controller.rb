30b0debb7325f8694c275e592c75f13f392a79aa
Antony Shimmin <a.shimmin@2009.ljmu.ac.uk>
2012-03-15 14:44:57 +0000

    With Paperclip

--- holiday/app/controllers/holidays_controller.rb
+++ holiday/app/controllers/holidays_controller.rb
@@ -2,7 +2,8 @@ class HolidaysController < ApplicationController
   def index
 @holiday = Holiday.all
 respond_to do |format|
-format.html # Send the page using HTMLformat.xml { render :xml => @holidays } # Send the page using XML
+format.html # Send the page using HTML
+format.xml { render :xml => @holidays } # Send the page using XML
 format.atom
 @holidays = Holiday.paginate :page => params[:page], :per_page => '12', :order => 'created_at DESC'
 end
@@ -33,7 +34,7 @@ if@holiday.save
     	redirect_to @holiday
 else
 	flash[:error] = "Was not Updated!!"
-  	redirect_to :action => "edit_holiday_path"
+    	redirect_to :action => "edit_holiday_path"
 end
 
 end
--- holiday/app/models/holiday.rb
+++ holiday/app/models/holiday.rb
@@ -3,6 +3,14 @@ belongs_to :user
 
   validates_presence_of :dateleaving
   validates_presence_of :datereturning
+  
+  has_attached_file :photo, :styles => { :small => "150x150>" },
+                    :url  => "/assets/holidays/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/holidays/:id/:style/:basename.:extension"
+
+ 
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
 end
 
